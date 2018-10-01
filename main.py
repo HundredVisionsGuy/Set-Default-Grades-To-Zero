c9db6e4663e89895212ed4d61eddba2259a8b8d9
HundredVisionsGuy <winikkc@hsd.k12.or.us>
2018-09-30 17:39:22 -0700

    adding addZeroes that just prints out the rows

--- main.py
+++ main.py
@@ -6,12 +6,22 @@
 import csv
 import os
 
-#
+# define functions
+def addZeroes(file):
+    f = open('docs/' + file)
+    csv_f = csv.reader(f)
+    for row in csv_f:
+        print row
+
 count = 0
 for (dirname, dirs, files) in os.walk('docs'):
    for filename in files:
        if filename.endswith('.csv') :
            count = count + 1
+       addZeroes(filename)
+       print '\n\nDone\n\n'
 print 'Files:', count
+
+
 
 # Loop through rows
