5c8b04b2db8138385719607b35bc3569f0b3f5f5
HundredVisionsGuy <winikkc@hsd.k12.or.us>
2018-09-30 17:29:59 -0700

    Adding csv and loop ability

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 # By HundredVisionsGuy
 # Takes a Canvas-generated CSV grade export and changes
 # all unscored assignments to 0
+
+import csv
+import os
+
+#
+count = 0
+for (dirname, dirs, files) in os.walk('docs'):
+   for filename in files:
+       if filename.endswith('.csv') :
+           count = count + 1
+print 'Files:', count
+
+# Loop through rows
